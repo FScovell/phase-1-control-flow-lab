c67290cd534d012dd147e118cb1a0c6ada30a37c
Frederick <admin@freddyscovell.com>
2021-12-13 16:21:41 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+
+function scuberGreetingForFeet(someNumber){
   // Write your code here!
+  if(someNumber <= 400){
+    return "This one is on me!";
+  } 
+  else if(someNumber < 2500){
+    return "I will gladly take your thirty bucks.";
+  }
+  else if(someNumber > 2500){
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(test){
   // Write your code here!
+  return test === 'NYC' ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipValue){
   // Write your code here!
+  switch(tipValue){
+    case 'generous':
+      return "Thank you so much.";
+      break;
+    case 'not as generous':
+      return "Thank you.";
+      break;
+    default:
+      return "Bye."
+      break;
+  }
 }
